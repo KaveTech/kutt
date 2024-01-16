b8ec801f0eeefe356a9240bf3acd3f4d9dee8d5b
Toni Grabulosa <tgrabulosa@kavehome.com>
2024-01-16 16:26:27 +0100

    comment footer content

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -18,48 +18,49 @@ const Footer: FC = () => {
   }, []);
 
   return (
-    <ColCenter
-      as="footer"
-      width={1}
-      backgroundColor="white"
-      p={isAuthenticated ? 2 : 24}
-    >
-      {!isAuthenticated && <ReCaptcha />}
-      <Text fontSize={[12, 13]} py={2}>
-        Made with love by{" "}
-        <ALink href="//thedevs.network/" title="The Devs" target="_blank">
-          The Devs
-        </ALink>
-        .{" | "}
-        <ALink
-          href="https://github.com/thedevs-network/kutt"
-          title="GitHub"
-          target="_blank"
-        >
-          GitHub
-        </ALink>
-        {" | "}
-        <ALink href="/terms" title="Terms of Service" isNextLink>
-          Terms of Service
-        </ALink>
-        {" | "}
-        <ALink href="/report" title="Report abuse" isNextLink>
-          Report Abuse
-        </ALink>
-        {publicRuntimeConfig.CONTACT_EMAIL && (
-          <>
-            {" | "}
-            <ALink
-              href={`mailto:${publicRuntimeConfig.CONTACT_EMAIL}`}
-              title="Contact us"
-            >
-              Contact us
-            </ALink>
-          </>
-        )}
-        .
-      </Text>
-    </ColCenter>
+    ""
+    // <ColCenter
+    //   as="footer"
+    //   width={1}
+    //   backgroundColor="white"
+    //   p={isAuthenticated ? 2 : 24}
+    // >
+    //   {!isAuthenticated && <ReCaptcha />}
+    //   <Text fontSize={[12, 13]} py={2}>
+    //     Made with love by{" "}
+    //     <ALink href="//thedevs.network/" title="The Devs" target="_blank">
+    //       The Devs
+    //     </ALink>
+    //     .{" | "}
+    //     <ALink
+    //       href="https://github.com/thedevs-network/kutt"
+    //       title="GitHub"
+    //       target="_blank"
+    //     >
+    //       GitHub
+    //     </ALink>
+    //     {" | "}
+    //     <ALink href="/terms" title="Terms of Service" isNextLink>
+    //       Terms of Service
+    //     </ALink>
+    //     {" | "}
+    //     <ALink href="/report" title="Report abuse" isNextLink>
+    //       Report Abuse
+    //     </ALink>
+    //     {publicRuntimeConfig.CONTACT_EMAIL && (
+    //       <>
+    //         {" | "}
+    //         <ALink
+    //           href={`mailto:${publicRuntimeConfig.CONTACT_EMAIL}`}
+    //           title="Contact us"
+    //         >
+    //           Contact us
+    //         </ALink>
+    //       </>
+    //     )}
+    //     .
+    //   </Text>
+    // </ColCenter>
   );
 };
 
